addb803d2ea243dfaa800c810adfdb8e5419d3c7
kamleshbiloniya <kamlesh@iitk.ac.in>
2020-04-30 13:07:59 +0530

    auto commit

--- find_max.c
+++ find_max.c
@@ -1,61 +1,109 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/time.h>
+#include<string.h>
+#include<pthread.h>
 #include<math.h>
-
+#define SEED 0x74
+pthread_t threads[64];
+#define MAX_THREADS 64
+#define TDIFF(start, end) ((end.tv_sec - start.tv_sec) * 1000000UL + (end.tv_usec - start.tv_usec))
 #define USAGE_EXIT(s) do{ \
-                             printf("Usage: %s <# of elements>\n %s\n", argv[0], s); \
+                             printf("Usage: %s <# of elements> <# of threads> \n %s\n", argv[0], s); \
                             exit(-1);\
                     }while(0);
+typedef struct thread_param{
 
-#define TDIFF(start, end) ((end.tv_sec - start.tv_sec) * 1000000UL + (end.tv_usec - start.tv_usec))
+                         int *array;
+                         int size;
+                         int skip;
+                         double max;
+                         int max_index;
+  }thread_param;
 
-double function(double element)
-{
-    return (sqrt(element) * sin(element));
+int checkprime(int x){
+  if(x==0)return 0;
+  for(int i=2; i<=sqrt(x);i++){
+    if(x%i==0)return 0;
+  }
+  return 1;
 }
 
-int find_max(int *array, int size, double *maxval)
-{
-     int ctr, index;
-     *maxval = function(array[0]);
-     index = 0;
-     for(ctr=1; ctr < size; ++ctr){
-           double x = function(array[ctr]);
-           if(x > *maxval){
-                *maxval = x;
-                index = ctr;
-           }
-     }          
-     return index;
+
+void *find_max_prime(void * arg){
+  static int count=0;
+  thread_param *param=(thread_param*)arg;
+  int i=count;
+  int max_p_index=-1;
+  while(i< (param->size)){
+    if(checkprime(param->array[i])&&max_p_index==-1)max_p_index=i;
+    else if(checkprime(param->array[i])&&param->array[i]>param->array[max_p_index])max_p_index=i;
+i+=param->skip;
+  }
+  param->max_index=max_p_index;
+  param->max=param->array[max_p_index];
+  count++;
+  return NULL;
 }
 
 int main(int argc, char **argv)
 {
-  struct timeval start, end;
-  int *a, num_elements, ctr;
-  double max;
-  if(argc !=2)
-           USAGE_EXIT("not enough parameters");
-
-  num_elements = atoi(argv[1]);
-  if(num_elements <=0)
-          USAGE_EXIT("invalid num elements");
-  
-  a = malloc(num_elements * sizeof(int));
-  if(!a){
+  int *ar,num_ele,num_th;
+   struct timeval start, end;
+  if(argc !=3)
+          USAGE_EXIT("not enough parameters");
+  num_th =atoi(argv[2]);
+  num_ele=atoi(argv[1]);
+  if(num_th <=0 || num_th > MAX_THREADS){
+        USAGE_EXIT("invalid num of threads");
+  }
+ar = malloc(num_ele * sizeof(int));
+if(!ar){
           USAGE_EXIT("invalid num elements, not enough memory");
   }
-  
-  for(ctr=0; ctr<num_elements; ++ctr)
-        a[ctr] = random();
-         
-  gettimeofday(&start, NULL);
+  srand(SEED);
+  for(int i=0;i<num_ele;i++){
+   ar[i]=random();
+  }
+    gettimeofday(&start, NULL);
+    thread_param *argp=(thread_param*)malloc(num_th*sizeof(thread_param));
+     bzero(argp,num_th*sizeof(thread_param));
+  int maximum=0;
+  for(int i=0; i<num_th; i++){
+    thread_param *arg=argp+i;
+    arg->array=ar;
+    arg->size=num_ele;
+    arg->skip=num_th;
+    arg->max_index=-1;
+  if(pthread_create(&threads[i], NULL, find_max_prime, (void*)arg) != 0){
+      printf("ERROR404\n");
+             perror("pthread_create");
+             exit(-1);
+       }
+  }
+  for(int i=0; i<num_th; i++){
+    pthread_join(threads[i], NULL);
+  }
 
-  ctr = find_max(a, num_elements, &max);
-  printf("Max = %.2f for %d at index=%d\n", max, a[ctr], ctr);
-  gettimeofday(&end, NULL);
-  
-  printf("Time taken = %ld microsecs\n", TDIFF(start, end));
-  free(a);
+  for(int i=0;i<num_th;i++){
+    thread_param *arg=argp+i;
+
+    if(arg->max>maximum)maximum=arg->max;
+  }
+    printf("Maximum prime number:%d\n",maximum);
+    gettimeofday(&end, NULL);
+      printf("Time taken = %ld microsecs\n", TDIFF(start, end));
+      free(argp);
+      free(ar);
+return 0;
 }
+
+/*
+what does it do?
+It finds maximum prime number. 
+compile:
+gcc -pthread find_max.c -lm
+
+run:
+./a.out 100 10
+*/
